4b37a7458b4e4c78004b325fe508e45a63b0f317
maaike <maaike@gmail.com>
2015-08-05 10:59:09 +0300

    initial setup - babel browserify

---
+++ src/components/container.babel.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Container = React.createClass({
+
+    render() {
+        return (<h1>Container</h1>);
+    }
+});
+
+export default Container;
---
+++ src/main.babel.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Container from './components/container.babel.js';
+
+React.render(
+    <Container/>,
+    document.getElementById('main')
+);
