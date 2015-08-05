4d7a16118ded57d93f711a1c7b0f241cb11d4dc1
maaike <maaike@gmail.com>
2015-08-05 17:55:37 +0300

    added date-picker

--- src/components/datepicker.babel.js
+++ src/components/datepicker.babel.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import {Button} from 'react-bootstrap';
+import ReactDatePicker from 'react-datepicker'
+import moment from 'moment'
 
 const DatePicker = React.createClass({
 
   render() {
     return (
-      <div className="datepicker row">
+      <div className="datepicker-row row">
 	      <div className="col-xs-12">
-		      On <Button className="btn-success">Pick a date!</Button> we read:
+		      <span className="pull-left">On</span>
+		      <ReactDatePicker
+			      placeholderText="Pick a date!"
+			      minDate={moment().subtract(5, 'years')}
+			      maxDate={moment()}
+			      selected={moment().subtract(5, 'years')}
+			      dateFormat="MMM D [,] YYYY"
+			      />
+		      <span>we read:</span>
 	      </div>
       </div>
         );
